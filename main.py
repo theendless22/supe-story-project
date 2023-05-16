3af3d9a53ce721ae6bf39e78c3fa82f273d2b8dd
SHRUTHI NAIDU <shruthinaidu@192-168-1-103.tpgi.com.au>
2023-05-16 16:52:23 +1000

    adding new stuff
    Signed-off-by: SHRUTHI NAIDU <shruthinaidu@192-168-1-103.tpgi.com.au>

---
+++ main.py
@@ -0,0 +1,16 @@
+#Create your superhero name
+import random
+import time
+
+superPowers = ['Flying', 'Super Strength', 'Telepathy', 'Super Speed', 'Unlimited Eating capability', 'Invisibility']
+superFirstName = ['Wonder','Super','Mad','Incredible', 'Astonishing', 'Decent', 'Stupendous', 'Organised', 'That Guy', 'Notably']
+superLastName = ['Woman', 'Man', 'Girl', 'Boy', 'Thing', 'Treat', 'Dude', 'Chick', 'Vikram', 'Deepika', 'Macho Lady','Stag']
+
+print("lets generate your random super hero name!")
+
+superName = random.choice(superFirstName) + random.choice(superLastName)
+
+print("your super-name is!")
+print(superName)
+print("your super power is:")
+print(random.choice(superPowers))
