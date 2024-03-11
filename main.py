aae137d534fbe0ffaf037e90a3ce92e6553c8bbd
SHRUTHI NAIDU <shruthinaidu91@gmail.com>
2024-03-11 23:09:59 +1100

    adding functions
    Signed-off-by: SHRUTHI NAIDU <shruthinaidu91@gmail.com>

--- main.py
+++ main.py
@@ -1,97 +1,104 @@
 # Create your superhero vs villain story
 import random
+import tkinter as tk
 
-# this is the super hero segment
 
-superPowers = [
-    "Flying",
-    "Super Strength",
-    "Telepathy",
-    "Super Speed",
-    "Unlimited Eating capability",
-    "Invisibility",
-    "Ice breath",
-]
-superFirstName = [
-    "Wonder",
-    "Super",
-    "Mad",
-    "Incredible",
-    "Astonishing",
-    "Decent",
-    "Stupendous",
-    "Organised",
-    "That",
-    "Notably",
-]
-superLastName = [
-    "Woman",
-    "Man",
-    "Girl",
-    "Boy",
-    "Thing",
-    "Treat",
-    "Dude",
-    "Chick",
-    "Vikram",
-    "Deepika",
-    "Macho Lady",
-    "Stag",
-    "Guy",
-]
+def generate_superhero():
+    # this is the super hero segment of our game.
+    superPowers = [
+        "Flying",
+        "Super Strength",
+        "Telepathy",
+        "Super Speed",
+        "Unlimited Eating capability",
+        "Invisibility",
+        "Ice breath",
+    ]
+    superFirstName = [
+        "Wonder",
+        "Super",
+        "Mad",
+        "Incredible",
+        "Astonishing",
+        "Decent",
+        "Stupendous",
+        "Organised",
+        "That",
+        "Notably",
+    ]
+    superLastName = [
+        "Woman",
+        "Man",
+        "Girl",
+        "Boy",
+        "Thing",
+        "Treat",
+        "Dude",
+        "Chick",
+        "Vikram",
+        "Uma",
+        "Macho Lady",
+        "Stag",
+        "Guy",
+    ]
 
-print("lets generate your random super hero name!")
+    print("lets generate your random super hero name!")
 
-superName = random.choice(superFirstName) + random.choice(superLastName)
+    superName = random.choice(superFirstName) + random.choice(superLastName)
+    superPower = random.choice(superPowers)
 
-print("your super-name is!")
-print(superName)
-print("your super power is:")
-print(random.choice(superPowers))
+    return f"Your Super Hero Name is {superName} and your power is {superPower}"
+
+    # print("your super-name is!")
+    # print(superName)
+    # print("your super power is:")
+    # print(random.choice(superPowers))
+
 
 # This is super villains segment
 
-villainPowers = [
-    "Flying",
-    "Super Strength",
-    "Telepathy",
-    "Super Speed",
-    "Unlimited Eating capability",
-    "Invisibility",
-    "Ice breath",
-]
 
-villan_first_name = [
-    "Dark",
-    "Blue",
-    "Unhappy",
-    "Lifeless",
-    "White",
-    "Unplanned",
-    "Rich",
-    "Psycotic",
-    "Trvial",
-    "Nutty",
-]
+def gerenate_super_villain():
+    villainPowers = [
+        "Flying",
+        "Super Strength",
+        "Telepathy",
+        "Super Speed",
+        "Unlimited Eating capability",
+        "Invisibility",
+        "Ice breath",
+    ]
+    villan_first_name = [
+        "Dark",
+        "Blue",
+        "Unhappy",
+        "Lifeless",
+        "White",
+        "Unplanned",
+        "Rich",
+        "Psycotic",
+        "Trvial",
+        "Nutty",
+    ]
+    villan_last_name = [
+        "Fly",
+        "Engineer",
+        "Pilot",
+        "Bitch",
+        "Freak",
+        "Student",
+        "Manager",
+        "Builder",
+        "Politician",
+        "Cricketer",
+    ]
 
-villan_last_name = [
-    "Fly",
-    "Engineer",
-    "Pilot",
-    "Bitch",
-    "Freak",
-    "Student",
-    "Manager",
-    "Builder",
-    "Politician",
-    "Cricketer",
-]
+    villainName = random.choice(villan_first_name) + random.choice(villan_last_name)
+    villainpower = random.choice(villainPowers)
 
-villainName = superName = random.choice(villan_first_name) + random.choice(
-    villan_last_name
-)
+    return f"Your arch nemesis is {villainName} and their power is {villainpower}"
 
-print("your arch nemesis is :O ")
-print(villainName)
-print("your nemesis has the power of:")
-print(random.choice(villainPowers))
+    # print("your arch nemesis is :O ")
+    # print(villainName)
+    # print("your nemesis has the power of:")
+    # print(random.choice(villainPowers))
